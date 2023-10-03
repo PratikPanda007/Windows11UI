3a8be80348b0f029eaf261adf38a704ece314436
PratikPanda <pratic.panda@gmail.com>
2023-10-03 17:15:21 +0530

    Added Base URL

--- script/TaskbarApps.js
+++ script/TaskbarApps.js
@@ -1,4 +1,5 @@
-var BASE_URL = "http://127.0.0.1:5500/";
+//var BASE_URL = "http://127.0.0.1:5500/";
+var BASE_URL = "https://pratikpanda007.github.io/Windows11UI/";
 const MyApps = [
   {
     name: "Edge",
